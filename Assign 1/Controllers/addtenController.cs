1629819c707bcd320993ba6e5afe2d65863808cf
Evneet Kaur <evneetkaur19@gmail.com>
2024-02-04 20:20:39 -0500

    Modified by evneet

--- Assign 1/Controllers/addtenController.cs
+++ Assign 1/Controllers/addtenController.cs
@@ -9,5 +9,17 @@ namespace Assign_1.Controllers
 {
     public class addtenController : ApiController
     {
+        /// <summary>
+        /// Now Add nine to user input.
+        /// </summary>
+        /// <returns>user input plus ten </returns>
+        /// <example>https://localhost:44390/api/addten/9 --> 19 </example>
+        /// <example>https://localhost:44390/api/addten/25 --> 35 </example>
+        /// <example>https://localhost:44390/api/addten/56 --> 66 </example>
+        /// <example>https://localhost:44390/api/addten/17 --> 27 </example>
+        public int Get(int id)
+        {
+            return id + 10;
+        }
     }
 }
--- Assign 1/Controllers/greetingsController.cs
+++ Assign 1/Controllers/greetingsController.cs
@@ -9,5 +9,37 @@ namespace Assign_1.Controllers
 {
     public class greetingsController : ApiController
     {
+        public string Post()
+        /// <summary>
+        /// Post request returns string "Hemllo World!"
+        /// </summary>
+        /// <return>"Hemllo World"</return>
+        /// <example> In command prompt: curl -d "public string Post()
+        /// <summary>
+        /// Post request returns string "Hello World!"
+        /// </summary>
+        /// <return>"Hello World"</return>
+        /// <example> In command prompt: curl -d "" https://localhost:44390/api/greetings -->"hello world!"</example>
+        {
+            return "hello world!";
+        }
+        public string Get(int id)
+        {   /// <summary>
+            /// Takes user input and concatenates that number to a greeting message
+            /// <param name="id"/>user input integer</param>
+            /// </summary>
+            /// <return>"greeting to id people</return>
+            /// <example>http://localhost:61608/api/greeting/3 --> greetings to 3 people!</example>
+            /// <example>http://localhost:61608/api/greeting/6 --> greetings to 6 people!</example>
+            /// <example>http://localhost:61608/api/greeting/0 --> greetings to 0 people!</example>
+
+            return $"greetings to {id} people!";
+        }
     }
-}
+}    
+    
+
+         
+   
+
+
